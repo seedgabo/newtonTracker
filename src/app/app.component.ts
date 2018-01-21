33b6aed2b4a235bc5375403b6b70fcbf6b13951d
seedgabo <seedgabo@gmail.com>
2018-01-21 11:44:59 -0500

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-// import { ListPage } from '../pages/list/list';
+import { ListPage } from '../pages/list/list';
 import { LoginPage } from '../pages/login/login';
 
 @Component({
@@ -17,8 +17,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any;
-
-  pages: Array<{ title: string, component: any }>;
+  pages: Array<{ title: string, component: any, icon: string }>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public backgroundmode: BackgroundMode, public codePush: CodePush, public api: Api) {
     this.initializeApp();
@@ -33,17 +32,14 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      // { title: 'List', component: ListPage }
+      { title: 'Home', component: HomePage, icon: 'home' },
+      { title: 'Seguimiento', component: ListPage, icon: 'locate' }
     ];
 
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      // this.statusBar.styleDefault()
       this.statusBar.styleLightContent()
       this.splashScreen.hide();
       this.backgroundmode.enable()
@@ -59,14 +55,13 @@ export class MyApp {
           this.backgroundmode.moveToBackground();
         }
       });
-      this.codePush.sync().subscribe((syncStatus) => console.log(syncStatus));
+
+      this.codePush.sync().subscribe((syncStatus) => console.log(syncStatus), console.warn);
 
     });
   }
 
   openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
   }
 
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,37 +1,134 @@
+import { BgProvider } from './../../providers/bg/bg';
+import { Api } from './../../providers/Api';
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-
+import { NavController, NavParams, Events, AlertController } from 'ionic-angular';
+import * as moment from 'moment';
+moment.locale('es');
+declare var L: any;
+declare var LatLng: any;
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  map
+  markers = {}
+  users = []
+  query = ""
+  cluster = L.markerClusterGroup()
+  constructor(public navCtrl: NavController, public navParams: NavParams, public events: Events, public alert: AlertController, public api: Api, public bg: BgProvider) {
+    events.subscribe('LocationCreated', (data) => {
+      this.markerUser(data.user, data.location.location);
+    })
+  }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
+  ionViewDidLoad() {
+    setTimeout(() => {
+      this.initMap();
+    }, 100)
+    this.getUsers();
+  }
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+  filter() {
+    if (this.query == "") {
+      return this.users = this.api.objects.users
+    }
+    var finder = this.query.toLowerCase();
+    this.users = this.api.objects.users.filter((u) => {
+      return u.full_name.toLowerCase().indexOf(finder) > -1;
+    })
+  }
 
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+  initMap() {
+    this.map = L.map('mapid', { zoomControl: false }).setView([47.7121724, -122.3246066], 13);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+    this.map.addLayer(this.cluster);
+    navigator.geolocation.getCurrentPosition((data) => {
+      this.map.panTo(new L.LatLng(data.coords.latitude, data.coords.longitude));
+    })
+  }
+
+
+  getUsers() {
+    this.api.load('users')
+      .then((users: any) => {
+        this.users = users;
+        users.forEach(u => {
+          console.log(u)
+          if (u.location)
+            this.markerUser(u, u.location);
+        });
+      })
+      .catch(this.api.Error);
+  }
+
+
+  locate() {
+    this.getDefaultLocation();
+
+    this.bg.locate().then((pos) => { })
+  }
+
+  centerInUser(user) {
+    var loc = user.location
+    if (loc)
+      this.map.panTo(new L.LatLng(loc.latitude, loc.longitude));
+
+    L.popup()
+      .setLatLng([loc.latitude, loc.longitude])
+      .setContent(this.htmlPopup(user))
+      .openOn(this.map);
+  }
+
+  markerUser(user, loc, pan = true) {
+    if (this.markers[user.id]) {
+      this.cluster.removeLayer(this.markers[user.id]);
+      if (pan)
+        this.map.panTo(new L.LatLng(loc.latitude, loc.longitude));
+      this.cluster.addLayer(this.markers[user.id])
+      this.cluster.refreshClusters(this.markers[user.id])
+    }
+    else {
+      var icon = L.divIcon({
+        className: 'user-icon',
+        iconSize: [50, 50],
+        html: `
+        <img src="${user.imagen}"  class="user-img-icon pulse"/>
+          <div class="label-map""><div>
+          ${user.full_name}
+          </div></div>
+          `
       });
+      if (pan)
+        this.map.panTo(new L.LatLng(loc.latitude, loc.longitude));
+      this.markers[user.id] = L.marker([loc.latitude, loc.longitude], { icon: icon });
+      this.cluster.addLayer(this.markers[user.id])
+      this.cluster.refreshClusters(this.markers[user.id])
+      this.markers[user.id].on('click', (ev) => {
+        var latlng = this.markers[user.id].getLatLng();
+        var popup = L.popup()
+          .setLatLng(latlng)
+          .setContent(this.htmlPopup(user)).openOn(this.map);
+      })
     }
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
+  htmlPopup(user) {
+    return `
+      <h6> ${user.full_name}</h6>
+      <span> ${moment(user.updated_at).format('LLLL')}</span>
+      <br>
+      <span><b>Departamento:</b>  ${user.departamento}</span>
+      <br>
+      <span><b>Cargo:</b> ${user.departamento}</>
+    `
+  }
+
+  getDefaultLocation() {
+    navigator.geolocation.getCurrentPosition((data) => {
+      this.map.panTo(new L.LatLng(data.coords.latitude, data.coords.longitude));
+    })
   }
 }
