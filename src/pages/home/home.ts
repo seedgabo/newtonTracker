4e50c67b4b58f5d92426739f9f113ce13f334915
gabriel <seedgabo@gmail.com>
2018-02-26 07:25:27 -0500

    fix timeout

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -23,7 +23,7 @@ export class HomePage {
     this.platform.ready().then(() => {
       var timeout = setTimeout(() => {
         this.codepush.restartApplication()
-      }, 1000 * 60);
+      }, 1000 * 60 * 10);
       window.codePush.sync((status) => {
         if (status == SyncStatus.UP_TO_DATE) {
           clearTimeout(timeout)
