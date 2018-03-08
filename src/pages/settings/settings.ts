cbca562dc1922edc8bf771b8fab29f037207ee2d
gabriel <seedgabo@gmail.com>
2018-03-08 08:46:59 -0500

    setting

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Api } from '../../providers/Api';
 import { SettingProvider } from '../../providers/setting/setting';
 
 @IonicPage()
